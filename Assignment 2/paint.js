221c469afd092a1471734e0709dc86367cef39b9
Kalob Morel <54212394+FadingEcho20@users.noreply.github.com>
2022-02-01 22:09:22 -0600

    program files for assignment 2

---
+++ Assignment 2/paint.js
@@ -0,0 +1,74 @@
+var paintColor;
+var paintSize;
+function setup() {
+  createCanvas(1920, 1080);
+  paintColor = color('black');
+  paintSize = 15;
+}
+
+function ui() {
+  //rectangles to view colors
+  let triangleHeight = 255;
+  let triangletop = 500;
+  strokeWeight(0);
+  fill('red');
+  square(0,0,50);
+  fill('orange');
+  square(0,50,50);
+  fill('yellow');
+  square(0,100,50);
+  fill('green');
+  square(0,150,50);
+  fill('cyan');
+  square(0,200,50);
+  fill('blue')
+  square(0,250,50);
+  fill('magenta');
+  square(0,300,50);
+  fill('brown');
+  square(0,350,50);
+  fill('white');
+  square(0,400,50);
+  fill('black');
+  square(0,450,50);
+  triangle(25, triangletop, 5, triangletop + triangleHeight, 45, triangletop + triangleHeight);
+}
+
+function draw() {
+  ui();
+  if(mouseIsPressed)
+  {
+    print('mouse is pressed');
+    if(mouseX < 50)
+    {
+      if(mouseY < 50)
+        paintColor = color('red');
+      else if(mouseY < 100)
+        paintColor = color('orange');
+      else if(mouseY < 150)
+        paintColor = color('yellow');
+      else if(mouseY < 200)
+        paintColor = color('green');
+      else if(mouseY < 250)
+        paintColor = color('cyan');
+      else if(mouseY < 300)
+        paintColor = color('blue');
+      else if(mouseY < 350)
+        paintColor = color('magenta');
+      else if(mouseY < 400)
+        paintColor = color('brown');
+      else if(mouseY < 450)
+        paintColor = color('white');
+      else if(mouseY < 500)
+        paintColor = color('black');
+      else if(mouseY <= 755)
+        paintSize = mouseY - 500;
+    }
+    else
+    {
+      stroke(paintColor);
+      strokeWeight(paintSize);
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+  }
+}
