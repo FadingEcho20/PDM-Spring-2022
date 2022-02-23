c91fc0cf31606a257a145eb7bf53e2ce040dd4be
Kalob Morel <54212394+FadingEcho20@users.noreply.github.com>
2022-02-22 19:55:40 -0600

    sampler implementation

--- Assignment 5/js/sketch.js
+++ Assignment 5/js/sketch.js
@@ -1,7 +1,43 @@
+// const { Tone } = require("tone/build/esm/core/Tone"); - auto generated when I called Tone
+let buttons;
+let sounds;
+let pitchShift;
+let soundNames;
+
+function preload() {
+  sounds = new Tone.Players({
+    space : 'sounds/27568__suonho__memorymoon-space-blaster-plays.wav',
+    explosion : 'sounds/156031__iwiploppenisse__explosion.mp3',
+    gameover : 'sounds/42349__irrlicht__game-over.wav',
+    talking : 'sounds/232175__szalonegacie__talking-people-2.mp3'
+  })
+  soundNames = ['space','explosion','gameover','talking']
+  buttons = [];
+}
+
 function setup() {
   createCanvas(400, 400);
+  pitchShift = new Tone.PitchShift().toDestination();
+  sounds.connect(pitchShift);
+
+  soundNames.forEach((text,index) => {
+    buttons[index] = createButton(text);
+    buttons[index].position(0, index * 20);
+    buttons[index].mousePressed(() => playSound(text));
+  });
+
+  slider = createSlider(-12, 12 , 0, 1);
+  slider.position(0,80);
 }
 
 function draw() {
   background(220);
+  textSize(12);
+  text('Press a button to play a sound.', 100, 45);
+  text('Pitch Control', 145, 95);
+  pitchShift.pitch = slider.value();
+}
+
+function playSound(whichSound) {
+  sounds.player(whichSound).start();
 }
