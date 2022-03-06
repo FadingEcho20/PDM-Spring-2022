b41b7465015201e518a623e959a5db1abb3b7bcc
Kalob Morel <54212394+FadingEcho20@users.noreply.github.com>
2022-03-06 11:14:11 -0600

    implemented one octave

--- Assignment 6/js/sketch.js
+++ Assignment 6/js/sketch.js
@@ -2,7 +2,7 @@
 let synth; 
 
 function preload() {
-  synth = new Tone.Synth().toDestination();
+  synth = new Tone.AMSynth().toDestination();
 }
 
 function setup() {
@@ -18,10 +18,30 @@ function draw() {
 // }
 
 function keyPressed() {
-  // if(keyCode === 70) {
-  //   print('F key pressed');
-  //   synth.triggerAttackRelease("C4", "8n");
-  // }
-  synth.triggerAttackRelease("C4", "8n");
-
+  if(keyCode === 81)
+    synth.triggerAttackRelease("C4", "8n");
+  if(keyCode === 50)
+    synth.triggerAttackRelease("C#4", "8n");
+  if(keyCode === 87)
+    synth.triggerAttackRelease("D4", "8n");
+  if(keyCode === 51)
+    synth.triggerAttackRelease("D#4", "8n");
+  if(keyCode === 69)
+    synth.triggerAttackRelease("E4", "8n");
+  if(keyCode === 82)
+    synth.triggerAttackRelease("F4", "8n");
+  if(keyCode === 53)
+    synth.triggerAttackRelease("F#4", "8n");
+  if(keyCode === 84)
+    synth.triggerAttackRelease("G4", "8n");
+  if(keyCode === 54)
+   synth.triggerAttackRelease("G#4", "8n");
+  if(keyCode === 89)
+    synth.triggerAttackRelease("A4", "8n");
+  if(keyCode === 55)
+   synth.triggerAttackRelease("A#4", "8n");
+  if(keyCode === 85)
+    synth.triggerAttackRelease("B4", "8n");
+  if(keyCode === 73)
+    synth.triggerAttackRelease("C5", "8n");
 }
