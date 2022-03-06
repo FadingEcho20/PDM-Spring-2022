c9a6336c60947d9eeb8ca72202a26e0997109078
Kalob Morel <54212394+FadingEcho20@users.noreply.github.com>
2022-03-06 10:42:17 -0600

    implement first tones

--- Assignment 6/js/sketch.js
+++ Assignment 6/js/sketch.js
@@ -1,7 +1,27 @@
+// const { Tone } = require("tone/build/esm/core/Tone"); - auto generated when I called Tone
+let synth; 
+
+function preload() {
+  synth = new Tone.Synth().toDestination();
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+}
+
+// function playSound(whichSound) {
+//   sounds.player(whichSound).start();
+// }
+
+function keyPressed() {
+  // if(keyCode === 70) {
+  //   print('F key pressed');
+  //   synth.triggerAttackRelease("C4", "8n");
+  // }
+  synth.triggerAttackRelease("C4", "8n");
+
 }
