2eb1a223afe03ac5be45ff4dbe13fe0a6f82ec50
Kalob Morel <54212394+FadingEcho20@users.noreply.github.com>
2022-03-08 18:05:01 -0600

    generate tone on mouse click

--- Assignment 7/js/sketch.js
+++ Assignment 7/js/sketch.js
@@ -1,43 +1,27 @@
-// const { Tone } = require("tone/build/esm/core/Tone"); - auto generated when I called Tone
-let synth; 
+let pitch; 
 
-function preload() {
-  synth = new Tone.AMSynth().toDestination();
-}
+let osc = new Tone.AMOscillator(pitch,'sine','sine').start()
+let gain = new Tone.Gain().toDestination();
+let pan = new Tone.Panner().connect(gain);
+let ampEnv = new Tone.AmplitudeEnvelope({
+  attack: 0.1,
+  decay: 0.2,
+  sustain: 1.0,
+  release: 0.8
+}).connect(pan);
+osc.connect(ampEnv);
 
 function setup() {
   createCanvas(400, 400);
+
+  pitch = 440;
 }
 
 function draw() {
   background(220);
 }
 
-function keyPressed() {
-  if(keyCode === 81)
-    synth.triggerAttackRelease("C4", "8n");
-  if(keyCode === 50)
-    synth.triggerAttackRelease("C#4", "8n");
-  if(keyCode === 87)
-    synth.triggerAttackRelease("D4", "8n");
-  if(keyCode === 51)
-    synth.triggerAttackRelease("D#4", "8n");
-  if(keyCode === 69)
-    synth.triggerAttackRelease("E4", "8n");
-  if(keyCode === 82)
-    synth.triggerAttackRelease("F4", "8n");
-  if(keyCode === 53)
-    synth.triggerAttackRelease("F#4", "8n");
-  if(keyCode === 84)
-    synth.triggerAttackRelease("G4", "8n");
-  if(keyCode === 54)
-   synth.triggerAttackRelease("G#4", "8n");
-  if(keyCode === 89)
-    synth.triggerAttackRelease("A4", "8n");
-  if(keyCode === 55)
-   synth.triggerAttackRelease("A#4", "8n");
-  if(keyCode === 85)
-    synth.triggerAttackRelease("B4", "8n");
-  if(keyCode === 73)
-    synth.triggerAttackRelease("C5", "8n");
+function mousePressed() {
+  Tone.start();
+  ampEnv.triggerAttackRelease('4n');
 }
