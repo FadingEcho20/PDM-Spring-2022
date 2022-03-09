ca0e84a15493c6ef8f1e200f7f9fec07e47daea3
Kalob Morel <54212394+FadingEcho20@users.noreply.github.com>
2022-03-08 23:23:12 -0600

    implement sound effect

--- Assignment 7/js/sketch.js
+++ Assignment 7/js/sketch.js
@@ -1,15 +1,19 @@
-let pitch; 
+//background music
+let crusher = new Tone.BitCrusher(2.5).toDestination();
+let synth = new Tone.Synth().connect(crusher);
 
-let osc = new Tone.AMOscillator(pitch,'sine','sine').start()
-let gain = new Tone.Gain().toDestination();
-let pan = new Tone.Panner().connect(gain);
-let ampEnv = new Tone.AmplitudeEnvelope({
-  attack: 0.1,
-  decay: 0.2,
-  sustain: 1.0,
-  release: 0.8
-}).connect(pan);
-osc.connect(ampEnv);
+//sound effect
+const phaser = new Tone.Phaser({
+	frequency: 1,
+	octaves: 5,
+	baseFrequency: 20
+}).toDestination();
+const phas = new Tone.AMSynth().connect(phaser);
+
+let noteNumber; 
+let lastFramePlayed;
+let note = ['F4','E4','C#4','D4'];
+let playEffect;
 
 function preload() {
   img = loadImage('./1s5vtryd7gl81.jpg');
@@ -17,17 +21,45 @@ function preload() {
 
 function setup() {
   createCanvas(400, 400);
-  
-
-  pitch = 440;
+  pitch = 500;
+  noteNumber = 0;
+  lastFramePlayed = 0;
+  playEffect = false;
 }
 
 function draw() {
   background(220);
   image(img,0,0,400,400);
+
+  // if(!playEffect) {
+  //   if(noteNumber == 3) {
+  //     if(lastFramePlayed + 15 == frameCount) {
+  //       lastFramePlayed = frameCount;
+  //       synth.triggerAttackRelease(note[noteNumber], .25);
+  //       noteNumber = 0;
+  //     }
+  //   }
+  //   else {
+  //     if(noteNumber == 2) {
+  //       if(lastFramePlayed + 7 == frameCount) {
+  //         lastFramePlayed = frameCount;
+  //         synth.triggerAttackRelease(note[noteNumber], .125);
+  //         noteNumber++;
+  //       }
+  //     }
+  //     else {
+  //       if(lastFramePlayed + 15 == frameCount) {
+  //         lastFramePlayed = frameCount;
+  //         synth.triggerAttackRelease(note[noteNumber], .25);
+  //         noteNumber++;
+  //       }
+  //     }
+  //   }
+  // }
 }
 
 function mousePressed() {
   Tone.start();
-  ampEnv.triggerAttackRelease('4n');
+  // playEffect = true;
+  phas.triggerAttackRelease(20, 1);
 }
